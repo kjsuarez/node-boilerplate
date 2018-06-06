e36f44166c35ca79a3d4e4c0bc1d472d2b59e16a
Kevin Suarez <kjsuarez94@gmail.com>
2018-06-06 10:49:43 -0700

    added mongoose and models

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose  = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/boilerplate-db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
