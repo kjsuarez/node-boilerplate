59112b8b69535df43c6ba64c8a8d0c198270959c
Kevin Suarez <kjsuarez94@gmail.com>
2018-06-07 00:22:16 -0700

    prep for angular app

--- app.js
+++ app.js
@@ -24,19 +24,20 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    return res.render('index');
 });
+
 
 module.exports = app;
